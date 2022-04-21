5fa557ee4b4e144b9a7d3ff294a1267334addcb6
cumbinar <cumbiweb@gmail.com>
2022-04-21 18:49:16 -0500

    correr un programa con run

--- python-01.py
+++ python-01.py
@@ -1,6 +1,26 @@
-3*3
+numero = 3*3
+print(numero)
 print('holacumbi')
 a = 20
 b = 30
 c = a+b
 print(c)
+
+saludo="Buenas noches: "
+nombre="Cumbi"
+edad=55
+print(saludo,nombre,edad)
+
+agua = 25
+luz = 30
+gas = 35
+web = 500
+
+servicios = agua + luz + gas + web
+
+descuento = servicios * 0.10
+
+valor_a_pagar = servicios - descuento
+
+
+print("el valor a pagar es  " + str(valor_a_pagar))

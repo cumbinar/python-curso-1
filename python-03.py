c3e2ecf6a731731cb8e33b861420b337851fe5f4
cumbinar <cumbiweb@gmail.com>
2022-04-22 19:32:41 -0500

    Diseño de un algoritmo que lea 3 números y calcula su promedio

--- python-03.py
+++ python-03.py
@@ -2,3 +2,6 @@ print('Bienvenido')
 nombre = input('ingrese el nombre: ')
 print('Hola don '+ nombre)
 input()
+
+
+
---
+++ python-04.py
@@ -0,0 +1,19 @@
+#Diseño de un algoritmo que lea 3 números y calcula su promedio
+
+print('algoritmo que lee 3 números y calcula su promedio')
+
+nombre = input('Ingresa tu nombre y oprime enter ')
+
+a = int(input('Ingrese el primer número: '))
+b = int(input('Ingrese el segundo número: '))
+c = int(input('Ingrese el tercer  número: '))
+
+suma = a + b + c
+
+promedio = suma / 3
+
+print('El promedio es:  ' + str(promedio))
+
+print('Muchas gracias por tu\nparticipación  '+nombre)
+
+

e1b915db7c2723d3531c544b14aa51c4ff2be57d
cumbinar <cumbiweb@gmail.com>
2022-04-25 19:26:50 -0500

    elevar al cuadrado la suma de dos números y el resultado dividirlo entre 3

---
+++ python-07.py
@@ -0,0 +1,8 @@
+''''elevar al cuadrado la suma de dos números y el resultado dividirlo entre 3'''
+
+a = int(input('ingresa el primer número y enter '))
+b = int(input('ingresa el segundo número y enter '))
+
+resultado = float((a+b)**2)/3
+
+print('El resultado es  ' + str(resultado))

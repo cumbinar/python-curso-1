67fa2b2907e3885128b05a29ca9b70c2f64604a2
cumbinar <cumbiweb@gmail.com>
2022-05-05 16:54:53 -0500

    modificado ej. 27

--- python-27.py
+++ python-27.py
@@ -1,6 +1,6 @@
-n = int(input('Valor hasta donde debo imprimir:  '))
-
 '''Escriba un algoritmo que, dado un valor N, imprima cada uno de los términos de la serie 2,5,7,10,12,15,17,..., hasta antes de N. Además que calcule la suma de los términos.'''
+
+n = int(input('Valor hasta donde se debe imprimir:  '))
 valor = 2
 pos = 1
 

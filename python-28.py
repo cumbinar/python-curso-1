3bdedaed8f3ffc20505ca7b50b7d99fbd6c483da
cumbinar <cumbiweb@gmail.com>
2022-05-05 18:56:28 -0500

    finish range

--- python-28.py
+++ python-28.py
@@ -7,5 +7,14 @@ print(a[3])
 for i in a:
     print(str(a[i]) + ' Cumbi')
 
-for i in range(5):
+for i in range(5):  #crea un rango de cero a antes del 5
     print('Hola Cumbi')
+
+for i in range(8, 15): #crea un rango del 8 a antes del 15
+    print(i)
+
+
+for i in range(0, 16, 3): #genera un rango entre el 0 y antes del 16 con un paso de 3
+    print(i)
+
+   

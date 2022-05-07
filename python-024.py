53ae8516b529c67f6f5c830b080a2d7be6ed05c4
cumbinar <cumbiweb@gmail.com>
2022-05-07 17:05:52 -0500

    Se modifica ingreso si es hijo del dueño

--- python-024.py
+++ python-024.py
@@ -1,11 +1,19 @@
 # asignar un valor booleano a una variable 
 
 print(' Si una persona es mayor de 12 años, \n puede pasar a la montana rusa')
+print('')
 edad = int(input('¿Cuántos años tienes?:  '))
 
 puedePasar = edad >= 12
+hijoDelMaquinista = 'si'
 
-if puedePasar: #si esto en True
+if puedePasar:  # si esto en True
     print('Puedes ingresar a la montaña rusa')
 else:
-    print('No puedes INGRESAR, hasta ser mayor de 12 años')
+    hijo = input('¿Eres hijo Del maquinista?: ')
+    if hijo == hijoDelMaquinista:
+        print('Ingresa porque eres hijo Del maquinista')
+    else:
+        print('No puedes INGRESAR, hasta ser mayor de 12 años')
+
+print('')

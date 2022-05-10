08196a6c9d1324802202ccf976fc04360c19604f
cumbinar <cumbiweb@gmail.com>
2022-05-10 18:03:38 -0500

    more funcctions

--- python-35.py
+++ python-35.py
@@ -7,3 +7,10 @@ def suma(a, b):
 result = suma(1, 2)
 
 print(result)  # result = 3
+
+numero1 = int(input('Ingrese un número entero:  '))
+numero2 = int(input('Ingrese OTRO número entero:  '))
+
+result2 = suma(numero1, numero2) #la función se puede reurilizar
+
+print(result2)

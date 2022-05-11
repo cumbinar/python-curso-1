434d9b431c9cc13021d0ff4a2725539b0add5ebd
cumbinar <cumbiweb@gmail.com>
2022-05-10 19:01:02 -0500

    una función se puede reutilizar

--- python-36.py
+++ python-36.py
@@ -5,3 +5,6 @@ def sumar(a, b): #se define la función
     print('La suma de los dos valores es:  ' + str(res))
 
 sumar(8, 13)    # se llama a la función
+sumar(800, -813) # se puede reutilizar la función
+sumar(2348, 89713)
+sumar(3.148, 1.618) 

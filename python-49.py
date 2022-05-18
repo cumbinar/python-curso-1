de9528d720873a195a4ea07d7cb14946da0b037a
cumbinar <cumbiweb@gmail.com>
2022-05-18 07:27:28 -0500

    El método split() divide una cadena en subcadenas y las devuelve almacenadas en una lista.

---
+++ python-49.py
@@ -0,0 +1,10 @@
+'''El método split() divide una cadena en subcadenas 
+y las devuelve almacenadas en una lista.'''
+
+cadena = input('Ingrese una oración corta: ')
+#Cumbitara es la tierra de los andes
+print()
+print(cadena.split(" ")) #el espacio es el parámetro para dividir
+#['Cumbitara', 'es', 'la', 'tierra', 'de', 'los', 'andes'] 
+
+

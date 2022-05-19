340e2ea611eff8ebf85c163b8678db86846525cb
cumbinar <cumbiweb@gmail.com>
2022-05-19 18:59:11 -0500

    conjuntos y operaciones

---
+++ python-54.py
@@ -0,0 +1,18 @@
+#conjuntos
+
+conjunto = set()
+
+conjunto.add(78)
+
+print(conjunto)  # imprime {78}
+
+conjunto.add("Cumbi")
+
+print(conjunto)  # imprime {'Cumbi', 78}
+
+frutas = set()
+frutas.add("Manzana")
+print(frutas)
+frutas.add('Pera')
+frutas.add('Papaya')
+print(frutas)  # imprime {'Manzana', 'Papaya', 'Pera'}

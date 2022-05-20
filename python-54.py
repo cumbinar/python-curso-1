1c8290c078bcdd3aff531c803fadf015a4e84733
cumbinar <cumbiweb@gmail.com>
2022-05-19 19:10:38 -0500

    conjuntos- diccionarios -clave => valor

--- python-54.py
+++ python-54.py
@@ -1,4 +1,4 @@
-#conjuntos
+#conjuntos- diccionarios -clave => valor
 
 conjunto = set()
 
@@ -16,3 +16,32 @@ print(frutas)
 frutas.add('Pera')
 frutas.add('Papaya')
 print(frutas)  # imprime {'Manzana', 'Papaya', 'Pera'}
+
+#conjunto con clave valor se conoce como diccionario
+
+'''Un diccionario en Python es una colección de elementos, donde cada uno tiene una llave key y un valor value. Los diccionarios se pueden crear con paréntesis {} separando con una coma cada par key: value. En el siguiente ejemplo tenemos tres keys que son el nombre, la edad y el documento.'''
+
+d1 = {
+    "Nombre": "Sara",
+    "Edad": 27,
+    "Documento": 1003882
+}
+print(d1) #imprime {'Nombre': 'Sara', 'Edad': 27, 'Documento': 1003882}
+
+
+'''Otra forma equivalente de crear un diccionario en Python es usando dict() e introduciendo los pares key: value entre paréntesis.'''
+
+d2 = dict([
+    ('Nombre', 'Sara'),
+    ('Edad', 27),
+    ('Documento', 1003882),
+])
+print(d2) #Imprime {'Nombre': 'Sara', 'Edad': '27', 'Documento': '1003882'}
+
+
+#También es posible usar el constructor dict() para crear un diccionario.
+
+d3 = dict(Nombre='Sara',
+          Edad=27,
+          Documento=1003882)
+print(d3) #Imprime {'Nombre': 'Sara', 'Edad': 27, 'Documento': 1003882}

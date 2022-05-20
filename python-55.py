e3794f8dc6d66337d73eac6fc35293f891a40b36
cumbinar <cumbiweb@gmail.com>
2022-05-20 18:27:30 -0500

    rganizar información

---
+++ python-55.py
@@ -0,0 +1,8 @@
+# organizar información
+
+listanombres = ['juan', 'cumbi', 'suzo']
+listaapellidos = ['guevara', 'melo', 'tara']
+
+contactos = [ listanombres, listaapellidos] # lista de listas
+
+print  (contactos[0] [2] + ' '+contactos[1][1])

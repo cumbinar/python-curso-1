2a0081974af7b1a134813a58ae66d9b7a7aab0e9
cumbinar <cumbiweb@gmail.com>
2022-05-20 18:44:26 -0500

    rganizar información listas diccionarios

--- python-55.py
+++ python-55.py
@@ -1,4 +1,4 @@
-# organizar información
+# organizar información listas diccionarios
 
 listanombres = ['juan', 'cumbi', 'suzo']
 listaapellidos = ['guevara', 'melo', 'tara']
@@ -6,3 +6,23 @@ listaapellidos = ['guevara', 'melo', 'tara']
 contactos = [ listanombres, listaapellidos] # lista de listas
 
 print  (contactos[0] [2] + ' '+contactos[1][1])
+
+
+registro = 'Eduardo;Guevara;Melo;19408240;62'
+
+lista = registro.split(';')
+print(lista)
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+print(lista[4])
+
+contacto1 = 'Cumbi;Guevara;Melo'
+contacto2 = 'Pedro;Pinto;Pico'
+contacto3 = 'Juan;Rico;Tara'
+
+listaregistro = [contacto1, contacto2, contacto3]
+print(listaregistro[0].split(';'))
+print(listaregistro[1].split(';'))
+print(listaregistro[0].split(';'))

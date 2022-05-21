5977cee615ba63696dba72f0c4bf7390172e8f74
cumbinar <cumbiweb@gmail.com>
2022-05-21 11:27:03 -0500

    Agenda con diccionario

---
+++ python-57.py
@@ -0,0 +1,39 @@
+#Agenda con diccionario
+
+def ingresar_contacto(listaregistros):
+    nombre = input('Ingrese el Nombre: ')
+    telefono = int(input('Ingrese el número de teléfono: '))
+    registro = {'nombre' : nombre, 'telefono' : telefono}
+    listaregistros.append(registro)
+    return listaregistros
+
+
+def mostrar_contactos(listaregistros):
+    for i in listaregistros:
+        print('CONTACTOS REGISTRADOS')
+        print(i)
+        print(listaregistros)
+        #print('Nombre: '+ i['nombre']+'  Teléfono: '+ str(i['telefono']))
+        print()
+
+
+def presentar_menu():
+    listaregistros = []
+    while(True):
+        print('AGENDA DE CONTACTOS')
+        print('1. Ingresar Contacto')
+        print('2. Mostrar Contactos')
+        print('3. Salir')
+        opc = int(input('Seleccione una opción: '))
+        match (opc):
+            case 1:
+                listaregistros = ingresar_contacto(listaregistros)
+            case 2:
+                mostrar_contactos(listaregistros)
+            case 3:
+                break
+            case _:
+                print('Opción no Válida')
+
+
+presentar_menu()
